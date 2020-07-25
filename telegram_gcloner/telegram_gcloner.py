e86605acfd36c1f3eab568e0b3ee7bbc290cb384
Roshan Connor <56798243+roshanconnor123@users.noreply.github.com>
2020-07-25 22:44:41 +0530

    Update telegram_gcloner.py

--- telegram_gcloner/telegram_gcloner.py
+++ telegram_gcloner/telegram_gcloner.py
@@ -119,7 +119,7 @@ def main():
     load_handlers(updater.dispatcher)
 
     updater.start_polling()
-    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Telegram gcloner first call, start.')
+    updater.bot.send_message(chat_id=config.USER_IDS[0], text='Hey sexy, Wanna clone some TBs?')
     updater.idle()
 
 
