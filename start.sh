a2de6ec44d5762a1e74bd70467f005d8d7ff2896
Nenokkadine <65813999+Nenokkadine@users.noreply.github.com>
2020-08-13 11:28:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+cd ./telegram_gcloner
+echo -e "[General]\npath_to_gclone = ./fclone\n\ntelegram_token = $BOT_TOKEN\nuser_ids = $USER_ID\ngroup_ids = $GRP_ID\n\ngclone_para_override = $GC_PARA_OVERRIDE">> config.ini
+chmod 777 config.ini
+cd ..
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
